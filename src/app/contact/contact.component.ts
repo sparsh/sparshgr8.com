4aee85e9d420a3a1aef91adb4fc93579187765cd
sparsh <sparshgr8@yahoo.com>
2017-03-16 02:04:34 +0530

    contact post api done
    next task was admin android app which is also done, for posting work
    and for contact data display

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Resource } from '../app.resource'
 
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 import { MdDialog, MdDialogRef } from '@angular/material';
 import { ValidationService } from '../validation.service'
 @Component({ 
@@ -11,8 +12,10 @@ import { ValidationService } from '../validation.service'
 export class ContactComponent {
   clicked: boolean = false;
   selectedOption: string;
+   contactRefrence :any;
+  contact:any = {};
   contactForm = new FormGroup({
-
+   
     "fullName": new FormControl('', [Validators.required]),
     "message": new FormControl('', [Validators.required]),
     "email": new FormControl('', [Validators.required, ValidationService.emailValidator])
@@ -20,16 +23,17 @@ export class ContactComponent {
   });
 
   constructor(private resource: Resource,
-    public dialog: MdDialog) {
-
+    public dialog: MdDialog,
+    af: AngularFire) {
+  this.contactRefrence = af.database.list('/contacts');
   }
 
-  submitContactValuesToServer() {
+  submitContactValuesToServer(contact) {
     this.clicked = true;
     if (this.contactForm.valid) {
-
+      this.contactRefrence.push(contact);
       let dialogRef = this.dialog.open(DialogResultExampleDialog, {
-     
+   
       });
       //submit values to server
       //  this.contactForm.reset();
