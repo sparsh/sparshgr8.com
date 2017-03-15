384e4a5206a6e1287e4232ddb5ee4989e089a42d
sparsh <sparshgr8@yahoo.com>
2017-03-16 04:51:33 +0530

    total wrong work detail will be changed in near future

--- src/app/work/work_details/work.details.component.ts
+++ src/app/work/work_details/work.details.component.ts
@@ -2,23 +2,45 @@ import { Component } from '@angular/core';
 import { Resource } from '../../app.resource'
 import { WorkComponent } from "../work.component"
 import { AppComponent } from "../../app.component"
+import {Router} from "@angular/router"
 import { WorkDetailModel } from "./work.details.model"
 @Component({
   templateUrl: './work.details.component.html',
-  providers:[WorkDetailModel]
+  providers: [WorkDetailModel]
 })
 export class WorkDetailsComponent {
 
+
+  innerWidth: any;
   constructor(private resource: Resource,
+  private router: Router,
     private selectedWork: WorkDetailModel,
     private appComponent: AppComponent) {
     console.log("this work is " + JSON.stringify(WorkComponent.selectedWork));
-   
+
     this.selectedWork = WorkComponent.selectedWork;
- this.appComponent.changeTitleTo(this.selectedWork.title);
+    if(this.selectedWork == null)
+    {
+      this.router.navigate(["home"]);
+      return;
+    }
+    this.appComponent.changeTitleTo(this.selectedWork.title);
+
+    this.innerWidth = (window.screen.width);
+  }
+
+
+  onResize(event: any) {
+    this.innerWidth = event.target.innerWidth;
+  }
+
+
+  getColumnOnBasisOfDeviceWidth() {
+
+    let columns: number = Math.floor(this.innerWidth / 300);
+    return columns == 0 ? 1 : columns;
 
 
   }
-
 
 }
